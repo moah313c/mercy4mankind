e595d7c38adc5f3aa58522923d07fc27cb929321
ahmedmoe <moeenahmed313c@gmail.com>
2019-03-14 11:51:13 -0400

    Corrected the js file.

---
+++ functionality.js
@@ -0,0 +1,105 @@
+var a, b, c;
+a=1; //front banner 
+b=2; //left banner 
+c=3; // right banner 
+
+function nextBanner(){
+
+if(a==1){
+    c=1;
+    a=2;
+    b=3;
+    //let yellow behind
+    document.getElementById("banner1").style.zIndex = 0;
+    document.getElementById("banner3").style.zIndex = 0;
+    document.getElementById("banner2").style.zIndex = -1;
+    document.getElementById("banner1").style.left = "-100vw";
+    document.getElementById("banner2").style.left = "100vw";
+    document.getElementById("banner3").style.left = "0vw";
+    //resetZIndex();
+    return;
+}
+
+if(a==2){
+    a=3;
+    b=1;
+    c=2;
+    document.getElementById("banner2").style.zIndex = 0;
+    document.getElementById("banner3").style.zIndex = 0;
+    document.getElementById("banner1").style.zIndex = -1;
+    document.getElementById("banner1").style.left = "100vw";
+    document.getElementById("banner2").style.left = "0vw";
+    document.getElementById("banner3").style.left = "-100vw";
+    document.getElementById("banner1").style.zIndex = 0;
+    return;
+}
+
+if(a==3){
+    a=1;
+    b=2;
+    c=3;
+    document.getElementById("banner1").style.zIndex = 0;
+    document.getElementById("banner2").style.zIndex = 0;
+    document.getElementById("banner3").style.zIndex = -1;
+    document.getElementById("banner1").style.left = "0vw";
+    document.getElementById("banner2").style.left = "-100vw";
+    document.getElementById("banner3").style.left = "100vw";
+    return;
+}
+}
+
+function leftBanner(){
+
+if(a==1){
+    a=3;
+    b=1;
+    c=2;
+    document.getElementById("banner1").style.zIndex = 0;
+    document.getElementById("banner2").style.zIndex = 0;
+    document.getElementById("banner3").style.zIndex = -1;
+    document.getElementById("banner1").style.left = "100vw";
+    document.getElementById("banner2").style.left = "0vw";
+    document.getElementById("banner3").style.left = "-100vw";
+    return;
+}
+
+if(a==2){
+    a=1;
+    b=2;
+    c=3;
+    document.getElementById("banner1").style.zIndex = 0;
+    document.getElementById("banner3").style.zIndex = 0;
+    document.getElementById("banner2").style.zIndex = -1;
+    document.getElementById("banner1").style.left = "0vw";
+    document.getElementById("banner2").style.left = "-100vw";
+    document.getElementById("banner3").style.left = "100vw";
+    return;
+}
+
+if(a==3){
+    a=2;
+    b=3;
+    c=1;
+    document.getElementById("banner2").style.zIndex = 0;
+    document.getElementById("banner3").style.zIndex = 0;
+    document.getElementById("banner1").style.zIndex = -1;
+    document.getElementById("banner1").style.left = "-100vw";
+    document.getElementById("banner2").style.left = "100vw";
+    document.getElementById("banner3").style.left = "0vw";
+    return;
+}
+}
+
+function mouseOverSubMenu(whichMenu){
+    document.getElementById(whichMenu).style.backgroundColor = "white";
+    document.getElementById(whichMenu).style.opacity = "0.5";
+    document.getElementById(whichMenu).style.color = "black";
+    return;
+}
+
+function mouseOutSubMenu(whichMenu){
+    document.getElementById(whichMenu).style.backgroundColor = "";
+    document.getElementById(whichMenu).style.opacity = "";
+    document.getElementById(whichMenu).style.color = "";
+    return;
+}
